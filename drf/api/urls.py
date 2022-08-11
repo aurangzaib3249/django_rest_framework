c32c115116e0d8c9f0f0bd51aac9e078065c7f67
Muhammad Aurangzaib <muhammad.aurangzaib@codefulcrum.com>
2022-08-11 19:37:47 +0500

    social_auth

--- drf/api/urls.py
+++ drf/api/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path("login",UserView.as_view(),name="login"),
-    path("",HomeView.as_view(),name="home"),
-    path("<str:pk>",HomeView.as_view(),name="home"),
+    path("/login",UserView.as_view(),name="login"),
+    path("home1/",HomeView.as_view(),name="home"),
+    path("home1/<str:pk>",HomeView.as_view(),name="home"),
 ]
