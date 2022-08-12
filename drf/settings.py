83daa13e2127a063502a75136880ecf58d442bc2
Muhammad Aurangzaib <muhammad.aurangzaib@codefulcrum.com>
2022-08-12 19:00:39 +0500

    add static files

--- drf/settings.py
+++ drf/settings.py
@@ -200,11 +200,11 @@ STATIC_ROOT = "staticfile/"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-SERVER_EMAIL = '************@gmail.com'
+EMAIL_USE_TLS=True
+EMAIL_USER_SSL=False
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = '**********@gmail.com'
-EMAIL_HOST_PASSWORD = '**********'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_SUBJECT_PREFIX = '****'
-DEFAULT_FROM_EMAIL = '*********@gmail.com'
+EMAIL_HOST_USER = 'easyfilesforu@gmail.com'
+EMAIL_HOST_PASSWORD = 'fhbznnjviiofxypc'
